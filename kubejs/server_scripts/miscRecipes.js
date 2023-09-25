434b682a81963fa4ac16b3d5ca57ceaec057b6a6
Frog <1152a0xy425@gmail.com>
2023-09-25 15:55:14 -0400

    new mods, some compat
    stonecutter crates,
    removed worldgen,
    tag for tomato soup/sauce

--- kubejs/server_scripts/miscRecipes.js
+++ kubejs/server_scripts/miscRecipes.js
@@ -18,9 +18,8 @@ ServerEvents.recipes((event) => {
             type: 'minecraft:crafting_shaped',
             group: 'wood_fired_oven',
             pattern: [
-                'I I',
-                'BIB',
-                'BWB',
+                'ICI',
+                'BBB',
             ],
             key: {
                 I: {
@@ -29,14 +28,14 @@ ServerEvents.recipes((event) => {
                 B: {
                     item: 'minecraft:bricks',
                 },
-                W: {
-                    item: 'minecraft:water_bucket',
+                C: {
+                    item: 'minecraft:cauldron',
                 },
             },
             result: {
                 item: 'bakery:kitchen_sink',
             },
         },
-    ).replaceIngredient('minecraft:water_bucket', 'minecraft:air') // FIXME: replaceIngredient doesn't work on Quilt
+    ) //.replaceIngredient('minecraft:water_bucket', 'minecraft:air') // FIXME: replaceIngredient doesn't work on Quilt
         .id('bakery:kitchen_sink');
 });
