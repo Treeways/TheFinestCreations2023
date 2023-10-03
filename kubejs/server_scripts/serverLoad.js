e465753988633701e8fc503ee444389f20a8e73c
Treeway7 <56928485+Treeway7@users.noreply.github.com>
2023-10-03 17:45:20 -0500

    Fixed server reload script

--- kubejs/server_scripts/serverLoad.js
+++ kubejs/server_scripts/serverLoad.js
@@ -1,7 +1,9 @@
 // priority: 10
 
+const $DedicatedServer = Java.loadClass('net.minecraft.server.dedicated.DedicatedServer');
+
 ServerEvents.loaded((event) => {
-    if (isServer()) {
+    if (event.server instanceof $DedicatedServer) {
         event.server.scheduleInTicks(48, () => {
             // Sometimes custom recipes don't load the first time. It's weird
             // event.server.tell('Reloading custom recipes...');
